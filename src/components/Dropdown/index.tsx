800a2f5777ecc56d184c2ee5b43e9bfac9f9e134
drew <andrewbraga1@gmail.com>
2020-07-11 23:50:25 -0300

    tela registrar montagem

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -25,46 +25,56 @@ const Dropdown = (props: any) => {
                 onValueChange={value => {handleSelectItem(value)}}
                 style={{
                 ...styles.picker,
-                iconContainer: {
-                  top: 8,
-                  right: 8,
-                  borderBottomRightRadius: 10,
-                  borderTopRightRadius: 10,
-                },
+                iconContainer: styles.iconContainer,
                 placeholder: styles.text
                 }}
-                useNativeAndroidPickerStyle={false}
                 Icon={() => {
                 return <Icon name="arrow-down" size={24} color="#FFF" />;
                 }}
-            />
+            ></RNPickerSelect>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 8,
-        paddingBottom: 8,
-        paddingHorizontal: 10,
+        //paddingTop: 8,
+        //paddingBottom: 8,
+        //paddingHorizontal: 10,
         backgroundColor: '#34CB79',
         height: 60,
         borderRadius: 10,
         color:'#fff',
         
     },
+    inputIOS: {
+      fontSize: 16,
+      color: '#fff',
+      paddingRight: 30, // to ensure the text is never behind the icon
+    },
+    inputAndroid: {
+      fontSize: 16,
+      color: '#fff',
+      paddingRight: 30,    // to ensure the text is never behind the icon
+    },
+    
     text: {
-        top:8,
-        left: 10,
-        justifyContent: 'center',
+        //top:8,
+        //left: 8,
+        //bottom:8,
+        //justifyContent: 'center',
         color: '#FFF',
         fontFamily: 'Roboto_500Medium',
         fontSize: 16,
       },
-    picker:{
-      flex: 1,
+    picker:{},
+    
+    iconContainer:{
+      zIndex:-10,
       height: 60,
       width: 60,
+      borderBottomRightRadius: 10,
+      borderTopRightRadius: 10,
       backgroundColor: 'rgba(0, 0, 0, 0.1)',
       justifyContent: 'center',
       alignItems: 'center'
--- src/pages/Records/index.tsx
+++ src/pages/Records/index.tsx
@@ -3,14 +3,45 @@ import {
     View,
     Text,
     TouchableOpacity,
+    ScrollView,
+    StyleSheet,
+    TextInput,
+    Platform,
+      KeyboardAvoidingView,
     } from 'react-native';
 import{ Feather as Icon } from '@expo/vector-icons';
 import  { useNavigation } from '@react-navigation/native';
 import api from '../../services/api';
-
-
+import Dropdown from '../../components/Dropdown';
+import Constants from 'expo-constants';
 
 const Records = () =>{
+
+    let inputRefs = {
+        firstTextInput: '',
+        favSport0: '',
+        favSport1: '',
+        lastTextInput: '',
+        favSport5: '',
+      };
+
+    const vehicles = [
+        {
+          label: 'Moto',
+          value: 'motos',
+          key: 'motos',
+        },
+        {
+          label: 'Carro',
+          value: 'carros',
+          key: 'carros',
+        },
+        {
+          label: 'Caminhão',
+          value: 'caminhoes',
+          key: 'caminhoes',
+        },
+      ];
     const navigation = useNavigation();
     
     useEffect(()=>{
@@ -21,21 +52,133 @@ const Records = () =>{
         navigation.goBack();
     }
 
-    
+            
     return (
-      <>
-        <View >
+        <>
+        
+        <View style={styles.container} >
             <TouchableOpacity onPress={handleNavigateBack}>
                 <Icon name="arrow-left" color="#34cb79" size={20}/>
             </TouchableOpacity>
                 
-            <Text > Estrutura records </Text>
+            <Text style={styles.title}>
+                Bem vindo.
+            </Text>
+            <Text style={styles.description}>
+                Cadastre um novo veículo.
+            </Text>
 
+            <View style={styles.itemsContainer}>
+                <Text style={styles.description_item}>
+                    Tipo de veículo
+                </Text>
+                <Dropdown itemsList = {vehicles} />
+            </View>
+            <View style={styles.itemsContainer}>
+                <Text style={styles.description_item}>
+                Marca
+                </Text>
+                <Dropdown itemsList = {vehicles} />
+            </View>
+            <View style={styles.itemsContainer}>
+                <Text style={styles.description_item}>
+                Modelo
+                </Text>
+                <Dropdown itemsList = {vehicles} />
+            </View>
+            <View style={styles.itemsContainer}>
+                <Text style={styles.description_item}>
+                    Ano
+                </Text>
+                <Dropdown itemsList = {vehicles} />
+            </View>
+            <View style={styles.itemsContainer}>
+                <Text style={styles.description_item}>
+                    Placa
+                </Text>
+                <TextInput
+                    ref={el => {
+                    inputRefs.firstTextInput = String(el);
+                    }}
+                    placeholder='XXX-0000'
+                    returnKeyType="next"
+                    enablesReturnKeyAutomatically
+                    onSubmitEditing={() => {
+                    inputRefs.favSport0;
+                    }}
+                    style={
+                    Platform.OS === 'ios'
+                        ? styles.inputIOS
+                        : styles.inputAndroid
+                    }
+                    blurOnSubmit={false}
+                />
+            </View>
             
         </View>
+                
+            
+     
       </>
     )
+    
 }
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingHorizontal: 32,
+      paddingTop: 20 + Constants.statusBarHeight,
+    },
+  
+    title: {
+      fontSize: 20,
+      fontFamily: 'Ubuntu_700Bold',
+      marginTop: 24,
+    },
+  
+    description: {
+      color: '#6C6C80',
+      fontSize: 16,
+      marginTop: 4,
+      fontFamily: 'Roboto_400Regular',
+    },
+    description_item: {
+      color: '#6C6C80',
+      fontSize: 16,
+      marginTop: 4,
+      marginBottom: 6,
+      fontFamily: 'Roboto_400Regular',
+    }, 
+    itemsContainer: {
+      marginTop: 8,
+      marginBottom: 8,
+      
+    },
+    
+        inputIOS: {
+          height: 60,
+          fontSize: 16,
+          paddingVertical: 12,
+          paddingHorizontal: 10,
+          borderWidth: 1,
+          borderColor: '#6C6C80',
+          borderRadius: 4,
+          color: 'black',
+          paddingRight: 30, // to ensure the text is never behind the icon
+        },
+        inputAndroid: {
+          height: 60,
+          fontSize: 16,
+          paddingHorizontal: 10,
+          paddingVertical: 8,
+          borderWidth: 1.5,
+          borderColor: '#6C6C80',
+          borderRadius: 8,
+          color: '#6C6C80',
+          paddingRight: 30, // to ensure the text is never behind the icon
+        },
+   
+  });
 
 export default Records;
